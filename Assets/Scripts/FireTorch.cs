ee54e57afdabebc2885466103a978dd6a5c855af
Syed Suleiman <87980933+eyal-zawlik@users.noreply.github.com>
2023-02-10 21:54:15 +0500

    part 1 modification
    added fire. assets for next room
    fixed grabbable items
    created new prefabs..

---
+++ Assets/Scripts/FireTorch.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class FireTorch : MonoBehaviour
+{
+    public static float StartFireDelay = 1;
+    
+    [Header("Reference")] [SerializeField] private GameObject fireParticle;
+
+    [Header("Audio Clip")] [SerializeField]
+    private AudioClip fireStartEffect;
+
+    [SerializeField] private AudioSource fireAudio;
+    [SerializeField] private bool isHandTorch;
+    
+    private bool _hasLookedAt;
+
+    private void Awake()
+    {
+        fireParticle.SetActive(false);
+        fireAudio.gameObject.SetActive(false);
+    }
+
+    public void Fire(bool isFire)
+    {
+        fireParticle.SetActive(isFire);
+    }
+
+    private void OnBecameVisible()
+    {
+        if (_hasLookedAt)
+        {
+            fireAudio.Play();
+            return;
+        }
+        if (isHandTorch) return;
+        _hasLookedAt = true;
+        StartFireDelay += .5f;
+        Invoke(nameof(StartFire), StartFireDelay);
+    }
+
+    private void OnBecameInvisible()
+    {
+        fireAudio.Pause();
+    }
+
+    private void StartFire()
+    {
+        fireParticle.SetActive(true);
+        fireAudio.gameObject.SetActive(true);
+        // give sound effect
+        StartFireDelay -= .5f;
+        AudioSource.PlayClipAtPoint(fireStartEffect, transform.position, .8f);
+        fireAudio.PlayDelayed(StartFireDelay);
+    }
+}
